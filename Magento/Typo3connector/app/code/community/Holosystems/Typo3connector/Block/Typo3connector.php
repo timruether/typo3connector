ffa3980db5322899849974c6d18b1c8fcab96409
Bjoern Jacob <bjoern.jacob@tritum.de>
2013-08-23 16:18:37 +0200

    ADD: add TYPO3 content via block in Magento

--- Magento/Typo3connector/app/code/community/Holosystems/Typo3connector/Block/Typo3connector.php
+++ Magento/Typo3connector/app/code/community/Holosystems/Typo3connector/Block/Typo3connector.php
@@ -6,6 +6,14 @@ class Holosystems_Typo3connector_Block_Typo3connector extends Mage_Core_Block_Te
         return parent::_prepareLayout();
     }
 
+	public function _toHtml() {
+		$html = parent::_toHtml();
+		if ($identifier = $this->getData('identifier')) {
+			$html .= $this->helper('typo3connector')->getContent($identifier);
+		}
+		return $html;
+    }
+
     public function getTypo3connector() {
         if (!$this->hasData('typo3connector')) {
             $this->setData('typo3connector', Mage::registry('typo3connector'));
