937c9f1282ea4572a18bdff556dc3b2433762d73
Sebastian Wagner <sebastian.wagner@tritum.de>
2013-09-03 10:36:27 +0200

    clear single cached block when saving connector entity

--- Magento/Typo3connector/app/code/community/Holosystems/Typo3connector/Model/Typo3connector.php
+++ Magento/Typo3connector/app/code/community/Holosystems/Typo3connector/Model/Typo3connector.php
@@ -6,7 +6,27 @@ class Holosystems_Typo3connector_Model_Typo3connector extends Mage_Core_Model_Ab
 	parent::_construct();
 	$this->_init('typo3connector/typo3connector');
     }
-  
+
+	/**
+	 * Clear cache for current identifier
+	 *
+	 * @return Holosystems_Typo3connector_Model_Typo3connector
+	 */
+	protected function cleanCache() {
+		Mage::app()->cleanCache(Holosystems_Typo3connector_Block_Typo3connector::CACHE_TAG . '_' . $this->getIdentifier());
+		return $this;
+	}
+
+	/**
+	 * Processing object before save data
+	 *
+	 * @return Holosystems_Typo3connector_Model_Typo3connector
+	 */
+	protected function _beforeSave() {
+		parent::_beforeSave();
+		$this->cleanCache();
+		return $this;
+	}
 
     /**
      * encrypt the data to be send, right now we only base64 encode it
