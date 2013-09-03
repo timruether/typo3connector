f7b8c22aa3b520f289133435a643ea37ec5cab7a
Sebastian Wagner <sebastian.wagner@tritum.de>
2013-09-03 16:11:58 +0200

    webservice for clearing cachetags of connector block

---
+++ Magento/Typo3connector/app/code/community/Holosystems/Typo3connector/Model/Typo3connector/Api.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Holosystems_Typo3connector extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category	Holosystems
+ * @package		Holosystems_Typo3connector
+ * @copyright	Copyright © 2013 holosystems ( http://www.holosystems.de/ )
+ * @license		http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Webservice connector model
+ *
+ * @category    Holosystems
+ * @package     Holosystems_Typo3connector
+ * @subpackage  Model
+ * @author      Sebastian Wagner <sebastian.wagner@tritum.de>
+ * @since       0.1.0
+ */
+class Holosystems_Typo3connector_Model_Typo3connector_Api
+	extends Mage_Api_Model_Resource_Abstract
+{
+
+	/**
+	 * @param int $identifier
+	 * @return string
+	 */
+	public function cleancache($identifier = 0) {
+		if($identifier > 0) {
+			Mage::log('cleancache: Clearing Cache for Identifier ' . (int)$identifier, NULL, 'success.log', TRUE);
+			Mage::app()->cleanCache(Holosystems_Typo3connector_Block_Typo3connector::CACHE_TAG . '_' . (int)$identifier);
+		} else {
+			Mage::log('cleancache: Missing identifier', Zend_Log::NOTICE, 'success.log', TRUE);
+		}
+		return 'identifier: ' . (int)$identifier;
+	}
+
+}
---
+++ Magento/Typo3connector/app/code/community/Holosystems/Typo3connector/Model/Typo3connector/Api/V2.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Holosystems_Typo3connector extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category	Holosystems
+ * @package		Holosystems_Typo3connector
+ * @copyright	Copyright © 2013 holosystems ( http://www.holosystems.de/ )
+ * @license		http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Webservice connector model
+ *
+ * @category    Holosystems
+ * @package     Holosystems_Typo3connector
+ * @subpackage  Model
+ * @author      Sebastian Wagner <sebastian.wagner@tritum.de>
+ * @since       0.1.0
+ */
+class Holosystems_Typo3connector_Model_Typo3connector_Api_V2
+	extends Holosystems_Typo3connector_Model_Typo3connector_Api
+{
+
+}
